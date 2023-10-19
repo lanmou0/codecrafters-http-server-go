d0c30a0ffb569ba3ef24f27f5f51442abaad1537
Nouaman Lamrahi <nouamanlamrahi@Nouamans-MacBook-Air.local>
2023-10-20 00:03:59 +0200

    stage 3

--- app/server.go
+++ app/server.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+	"io"
 	"net"
 	"os"
 	"strings"
@@ -11,40 +12,46 @@ func main() {
 	// You can use print statements as follows for debugging, they'll be visible when running tests.
 	fmt.Println("Logs from your program will appear here!")
 
-	var reqBody []byte;
 	var escape = "\r\n"
 
-	// Uncomment this block to pass the first stage
 	l, err := net.Listen("tcp", "0.0.0.0:4221")
 	if err != nil {
 		fmt.Println("Failed to bind to port 4221")
 		os.Exit(1)
 	}
+	fmt.Println("listening on Port 4221")
 	
-	req, err := l.Accept()
+	conn, err := l.Accept()
 	if err != nil {
 		fmt.Println("Error accepting connection: ", err.Error())
 		os.Exit(1)
 	}
+	defer conn.Close()
+	fmt.Println("Connection Accepted")
 
-	_, err = req.Read(reqBody)
+	buf, err := io.ReadAll(conn)
 	if err != nil {
 		fmt.Println("Error reading request: ", err.Error())
 		os.Exit(1)
 	}
 
-	s := strings.Split(string(reqBody), escape)
+  var resp = "HTTP/1.1 200 OK\r\n\r\n";
 
-	var resp string;
-	if(strings.Split(s[0], " ")[1] == "/") {
-		resp = "HTTP/1.1 200 OK\r\n\r\n"
-	} else {
-		resp = "HTTP/1.1 404 Not Found\r\n\r\n"
+	s := strings.Split(string(buf), escape)
+
+	for _, line := range s {
+		fmt.Println(line)
 	}
 
-	_, err = req.Write([]byte(resp))
+	if(strings.Split(s[0], " ")[1] != "/") {
+		resp = "HTTP/1.1 404 Not Found\r\n\r\n"
+	}
+	fmt.Println("Writing response", resp)
+
+	_, err = conn.Write([]byte(resp))
 	if err != nil {
 		fmt.Println("Error writing request: ", err.Error())
 		os.Exit(1)
 	}
+	fmt.Println("Response sent")
 }
