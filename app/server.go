94f66f66c7a460a25bd7568b5e12b644a9fa8763
Nouaman Lamrahi <nouamanlamrahi@Nouamans-MacBook-Air.local>
2023-10-19 23:18:01 +0200

    stage 3

--- app/server.go
+++ app/server.go
@@ -4,13 +4,15 @@ import (
 	"fmt"
 	"net"
 	"os"
+	"strings"
 )
 
 func main() {
 	// You can use print statements as follows for debugging, they'll be visible when running tests.
 	fmt.Println("Logs from your program will appear here!")
 
-	var b []byte;
+	var reqBody []byte;
+	var escape = "\r\n"
 
 	// Uncomment this block to pass the first stage
 	l, err := net.Listen("tcp", "0.0.0.0:4221")
@@ -25,19 +27,24 @@ func main() {
 		os.Exit(1)
 	}
 
-	_, err = req.Read(b)
+	_, err = req.Read(reqBody)
 	if err != nil {
 		fmt.Println("Error reading request: ", err.Error())
 		os.Exit(1)
 	}
 
-	var resp = []byte("HTTP/1.1 200 OK\r\n\r\n")
+	s := strings.Split(string(reqBody), escape)
 
-	_, err = req.Write(resp)
+	var resp string;
+	if(strings.Split(s[0], " ")[1] == "/") {
+		resp = "HTTP/1.1 200 OK\r\n\r\n"
+	} else {
+		resp = "HTTP/1.1 404 Not Found\r\n\r\n"
+	}
+
+	_, err = req.Write([]byte(resp))
 	if err != nil {
 		fmt.Println("Error writing request: ", err.Error())
 		os.Exit(1)
 	}
-
-	req.Close()
 }
